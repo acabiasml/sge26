d785ef751c3cf979ef6ea87a2902717ef8707101
Acabias Luiz <acabias@outlook.com>
2026-08-22 21:21:59 -0400

    fix: integrar paginação à autoria dos PDFs

--- resources/views/reports/partials/document-footer.blade.php
+++ resources/views/reports/partials/document-footer.blade.php
@@ -11,13 +11,13 @@
     @endif
     <div>
         Documento emitido pelo Beabá. Autenticidade: {{ $issuedDocument->verification_code }}.
-        Emitido em {{ $issuedDocument->issued_at?->timezone('America/Sao_Paulo')->format('d/m/Y H:i:s') }} por {{ $issuer }}.
+        Emitido em {{ $issuedDocument->issued_at?->timezone('America/Sao_Paulo')->format('d/m/Y H:i:s') }}.
     </div>
 </footer>
 <script type="text/php">
     if (isset($pdf, $fontMetrics)) {
         $font = $fontMetrics->getFont('DejaVu Sans', 'normal');
-        $label = 'Página {PAGE_NUM} de {PAGE_COUNT}';
+        $label = @json('por '.$issuer.'. Página {PAGE_NUM} de {PAGE_COUNT}');
         $size = 8.25;
         $width = $fontMetrics->getTextWidth($label, $font, $size);
         $pdf->page_text(($pdf->get_width() - $width) / 2, $pdf->get_height() - 13, $label, $font, $size, [0.37, 0.35, 0.33]);
