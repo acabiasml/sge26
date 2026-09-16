4222e211cee949081bf45577958f7f385189545a
Acabias Luiz <acabias.luiz@ufr.edu.br>
2026-09-16 12:30:33 -0400

    iguala margem inferior dos relatorios pdf

--- resources/views/reports/school-class-schedules.blade.php
+++ resources/views/reports/school-class-schedules.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <style>
-        @page { size: A4 landscape; margin: 126px 18px 68px; }
+        @page { size: A4 landscape; margin: 126px 18px 96px; }
         body { font-family: 'Atkinson Hyperlegible Next', DejaVu Sans, sans-serif; color: #2f241f; font-size: 11px; line-height: 1.18; }
         @include('reports.partials.letterhead-styles')
         .letterhead-repeat { top: -114px; padding-bottom: 4px; }
